d4371e7056cf3de78776f1c5b8eba8988970aef2
spencer kelly <spencermountain@gmail.com>
2019-09-16 17:13:25 -0400

    transition working, thames river line

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const Deck = require('@deck.gl/core').Deck
+// const FlyToInterpolator = require('@deck.gl/FlyToInterpolator')
 const GeoJsonLayer = require('@deck.gl/layers').GeoJsonLayer
 const scaleLinear = require('./scale')
+
+const latitude = 43.6542
+const longitude = -79.5074
 
 const data = {
   lakeOntario: './data/lake-ontario.json',
@@ -10,7 +14,8 @@ const data = {
   stClairNorth: './data/st-clair-north.json',
   stClairSouth: './data/st-clair-south.json',
   niagaraSouth: './data/niagara-south.json',
-  niagaraNorth: './data/niagara-north.json'
+  niagaraNorth: './data/niagara-north.json',
+  thamesRiver: './data/thames-river.json'
 }
 
 let scale = scaleLinear({
@@ -21,8 +26,8 @@ let scale = scaleLinear({
 // cn tower - 553 m
 
 const INITIAL_VIEW_STATE = {
-  latitude: 43.6542,
-  longitude: -79.5074,
+  latitude: latitude,
+  longitude: longitude,
   zoom: 6.8,
   bearing: -45,
   pitch: 50
@@ -38,7 +43,8 @@ let layers = [
   { id: 'niagara-south', elevation: 173 },
   { id: 'niagaraSouth', elevation: 170 },
   { id: 'niagaraNorth', elevation: 76 },
-  { id: 'lakeOntario', elevation: 74 }
+  { id: 'lakeOntario', elevation: 74 },
+  { id: 'thamesRiver', elevation: 74 }
 ]
 layers = layers.map(o => {
   return new GeoJsonLayer({
@@ -61,8 +67,18 @@ let deck = new Deck({
 })
 
 setTimeout(() => {
-  console.log('hi')
-  deck.viewState.transitionDuration = 8000
-  deck.viewState.bearing = 20
+  deck.setProps({
+    viewState: {
+      latitude: latitude,
+      longitude: longitude,
+      bearing: 20,
+      zoom: 6.8,
+      pitch: 50,
+      transitionEasing: function(t) {
+        return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1
+      },
+      transitionDuration: 3000
+    }
+  })
 }, 4000)
 // document.body.style.margin = '0px'
