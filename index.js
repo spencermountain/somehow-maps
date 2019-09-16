cfe4af1b917918a40c29ff5634d9068b57581bb0
spencer kelly <spencermountain@gmail.com>
2019-09-16 15:52:36 -0400

    great lakes done

--- index.js
+++ index.js
@@ -1,71 +1,67 @@
 const Deck = require('@deck.gl/core').Deck
-const layers = require('@deck.gl/layers')
-const GeoJsonLayer = layers.GeoJsonLayer
-const PolygonLayer = layers.PolygonLayer
+const GeoJsonLayer = require('@deck.gl/layers').GeoJsonLayer
+const scaleLinear = require('./scale')
 
-// source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
-const lakeOntario = './data/lake-ontario.json'
-const lakeHuron = './data/lake-huron.json'
-const lakeErie = './data/lake-erie.json'
-const lakeStClair = './data/lake-st-clair.json'
+const data = {
+  lakeOntario: './data/lake-ontario.json',
+  lakeHuron: './data/lake-huron.json',
+  lakeErie: './data/lake-erie.json',
+  lakeStClair: './data/lake-st-clair.json',
+  stClairNorth: './data/st-clair-north.json',
+  stClairSouth: './data/st-clair-south.json',
+  niagaraSouth: './data/niagara-south.json',
+  niagaraNorth: './data/niagara-north.json'
+}
+
+let scale = scaleLinear({
+  world: [0, 30000],
+  minmax: [0, 180]
+})
+
+// cn tower - 553 m
 
 const INITIAL_VIEW_STATE = {
   latitude: 43.6542,
   longitude: -79.5074,
   zoom: 6.8,
-  bearing: -20,
+  bearing: 70,
   pitch: 50
 }
 
-new Deck({
-  initialViewState: INITIAL_VIEW_STATE,
-  controller: true,
-  layers: [
-    new GeoJsonLayer({
-      id: 'lakeOntario',
-      data: lakeOntario,
-      stroked: false,
-      filled: true,
-      extruded: true,
-      lineWidthMinPixels: 2,
-      opacity: 1,
-      getElevation: 10000,
-      getFillColor: [70, 130, 180]
-    }),
-    new GeoJsonLayer({
-      id: 'lakeHuron',
-      data: lakeHuron,
-      stroked: false,
-      filled: true,
-      extruded: true,
-      lineWidthMinPixels: 2,
-      opacity: 1,
-      getElevation: 30000,
-      getFillColor: [70, 130, 180]
-    }),
-    new GeoJsonLayer({
-      id: 'lakeErie',
-      data: lakeErie,
-      stroked: false,
-      filled: true,
-      extruded: true,
-      lineWidthMinPixels: 2,
-      opacity: 1,
-      getElevation: 30000,
-      getFillColor: [70, 130, 180]
-    }),
-    new GeoJsonLayer({
-      id: 'lakeStClair',
-      data: lakeStClair,
-      stroked: false,
-      filled: true,
-      extruded: true,
-      lineWidthMinPixels: 2,
-      opacity: 1,
-      getElevation: 30000,
-      getFillColor: [70, 130, 180]
-    })
-  ]
+let layers = [
+  { id: 'lakeHuron', elevation: 176 },
+  { id: 'stClairNorth', elevation: 175 },
+  { id: 'lakeStClair', elevation: 175 },
+  { id: 'stClairSouth', elevation: 174 },
+  { id: 'lakeErie', elevation: 173 },
+  { id: 'niagara-south', elevation: 173 },
+  { id: 'niagaraSouth', elevation: 170 },
+  { id: 'niagaraNorth', elevation: 76 },
+  { id: 'lakeOntario', elevation: 74 }
+]
+layers = layers.map(o => {
+  return new GeoJsonLayer({
+    id: o.id,
+    data: data[o.id],
+    stroked: false,
+    filled: true,
+    extruded: true,
+    lineWidthMinPixels: 2,
+    opacity: 1,
+    getElevation: scale(o.elevation),
+    getFillColor: [70, 130, 180]
+  })
 })
 
+let deck = new Deck({
+  initialViewState: INITIAL_VIEW_STATE,
+  controller: true,
+  layers: layers
+})
+
+setTimeout(() => {
+  console.log('hi')
+  deck.viewState.transitionDuration = 8000
+  deck.viewState.bearing = 20
+}, 4000)
 // document.body.style.margin = '0px'
